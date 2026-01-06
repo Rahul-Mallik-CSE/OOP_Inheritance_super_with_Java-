3409652be2273f0a82ab8e7e758093967dfa4aa1
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-06 11:19:41 +0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+
+class A{
+    A(){
+        System.out.println("A's Constructor");
+    }
+}
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
